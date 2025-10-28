5cfc7e784d95a69a10568d96809d179560709056
Tomáš Ivan <tomas.ivan.it@gmail.com>
2025-10-28 11:53:40 +0100

    Uprava modulu tvary.stvorec

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from tvary import stvorec
+from tvary import obdlznik
 print(f"Obvod štvorca o veľkosti 10 je {stvorec.obvod(10)}")
+print(f"Obvod obdĺžnika o veľkosti 10 x 15 je {obdlznik.obvod(10, 15)}")
